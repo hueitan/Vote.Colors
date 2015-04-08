7b67aebc2eb6c11e014fa85bbfd5c4940bca23a3
huei <huei@hueis-MacBook-Air.local>
2015-04-08 14:18:36 +0800

    Fix async/sync calling http (need to use async ... later)

--- js/controllers.js
+++ js/controllers.js
@@ -18,40 +18,42 @@ angular.module('myApp.controllers', []).
         $http.get('data/color.json')
             .success(function (data) {
                 $scope.colors = data;
-            });
 
-        $http.get('data/vote.json')
-            .success(function (data) {
-                $scope.votes = data;
+                $http.get('data/vote.json')
+                    .success(function (data) {
+                        $scope.votes = data;
 
-                // Initialization
-                totalVote = $scope.votes.length;
+                        // Initialization
+                        totalVote = $scope.votes.length;
 
-                for (var i = 0; i < $scope.colors.length; i++) {
-                    $scope.colors[i].numberOfVote = 0;
-                }
-
-                // Calcuate the vote
-                for (var idx = 0; idx < $scope.votes.length; idx++) {
-                    for (var index = 0; index < $scope.colors.length; index++) {
-                        if (($scope.votes[idx].vote).toLowerCase() === ($scope.colors[index].name).toLowerCase()) {
-                            $scope.colors[index].numberOfVote++;
-                            if ($scope.colors[index].numberOfVote > largestVote) {
-                                largestcolor = $scope.colors[index].hex;
-                                largestVote = $scope.colors[index].numberOfVote;
-                            }
-                            break;
+                        for (var i = 0; i < $scope.colors.length; i++) {
+                            $scope.colors[i].numberOfVote = 0;
                         }
-                    }
-                }
 
-                // Calculate Number of Vote for every color
-                for (var index = 0; index < $scope.colors.length; index++) {
-                    $scope.colors[index].numberOfVote = $scope.colors[index].numberOfVote / totalVote * 100;
-                }
+                        // Calcuate the vote
+                        for (var idx = 0; idx < $scope.votes.length; idx++) {
+                            for (var index = 0; index < $scope.colors.length; index++) {
+                                if (($scope.votes[idx].vote).toLowerCase() === ($scope.colors[index].name).toLowerCase()) {
+                                    $scope.colors[index].numberOfVote++;
+                                    if ($scope.colors[index].numberOfVote > largestVote) {
+                                        largestcolor = $scope.colors[index].hex;
+                                        largestVote = $scope.colors[index].numberOfVote;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
 
-                $scope.largestcolor = largestcolor;
-                $scope.largestVote = largestVote;
-                $scope.totalVote = totalVote;
+                        // Calculate Number of Vote for every color
+                        for (var index = 0; index < $scope.colors.length; index++) {
+                            $scope.colors[index].numberOfVote = $scope.colors[index].numberOfVote / totalVote * 100;
+                        }
+
+                        $scope.largestcolor = largestcolor;
+                        $scope.largestVote = largestVote;
+                        $scope.totalVote = totalVote;
+                    });
             });
+
+
     }]);
